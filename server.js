ad43c835a5772ba78eb7b97fb53d58e3fefd6ad0
Doğukan <dogu_kutluay_1999@hotmail.com>
2022-04-11 22:31:59 +0300

    c

--- server.js
+++ server.js
@@ -1,49 +1,25 @@
-//http
-const { createServer } = require('http');
-//express import
+const { mongoDbConnection } = require('./mongodb/mongoDbConnection');
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
 
-//cors import
-const cors = require('cors');
-
-//custom errror import
-
-//routers import
 const router = require('./routes/index');
 
-//dotenv  import
-const dotenv = require('dotenv');
-
-//create server
-const httpServer = createServer(app);
-
-//dotenv config
 dotenv.config({
   path: './config/env/config.env',
 });
 
-//mongodb connection import
-const { mongoDbConnection } = require('./mongoDb/mongoDbConnection');
-
-//mongodb start
 mongoDbConnection();
 
-//cors use
 app.use(cors());
-//express json and urlencoded
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//v1
 
 app.use('/api/v1', router);
-//for get file route
 
-//app start
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
-// Socket setup
-
-httpServer.listen(PORT, 'localhost', () => {
-  console.log('server listening', `http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`****server started ${PORT}****`);
 });
